5cdb6509946d16318550c275bb97304736af145f
Ed <root@systemalert.org>
2026-05-31 18:52:01 +0200

    IMPL reverse insertion sort

--- algos/src/sorting.rs
+++ algos/src/sorting.rs
@@ -1,9 +1,16 @@
-pub fn insertion_sort(x: &mut [i32]) {
+pub fn insertion_sort(x: &mut [i32], reverse: bool) -> &mut [i32] {
     for i in 1..x.len() {
         let mut j = i;
-        while j > 0 && x[j - 1] > x[j] {
+        while j > 0
+            && (if reverse {
+                x[j - 1] > x[j]
+            } else {
+                x[j - 1] < x[j]
+            })
+        {
             x.swap(j - 1, j);
             j -= 1;
         }
     }
+    x
 }
