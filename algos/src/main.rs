793da2e0ed4e91265eb447940e5f39342933df81
Ed <root@systemalert.org>
2026-06-01 17:50:47 +0200

    test new sorting func

--- algos/src/main.rs
+++ algos/src/main.rs
@@ -2,6 +2,6 @@ mod sorting;
 use sorting::insertion_sort;
 
 fn main() {
-    let mut my_list = [1, 3, 2, 6, 5, 4];
-    println!("{:?}", insertion_sort(&mut my_list));
+    let mut my_list = [6, 3, 2, 1, 5, 4];
+    println!("{:?}", insertion_sort(&mut my_list, true));
 }
