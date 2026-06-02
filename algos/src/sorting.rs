a2ad76f40190e93c477703c5f54cdd461a105b12
Ed <root@systemalert.org>
2026-06-02 17:59:41 +0200

    IMPL merge-sort

--- algos/src/sorting.rs
+++ algos/src/sorting.rs
@@ -14,3 +14,47 @@ pub fn insertion_sort(x: &mut [i32], reverse: bool) -> &mut [i32] {
     }
     x
 }
+
+fn merge(arr: &mut [i32], p: usize, q: usize, r: usize) {
+    let n_left = q - p + 1;
+    let n_right = r - q;
+    let left: Vec<i32> = Vec::from(&arr[p..p + n_left]);
+    let right: Vec<i32> = Vec::from(&arr[p + n_left..p + n_left + n_right]);
+
+    let mut i = 0;
+    let mut j = 0;
+    let mut k = p;
+
+    while i < n_left && j < n_right {
+        if left[i] <= right[j] {
+            arr[k] = left[i];
+            i += 1;
+        } else {
+            arr[k] = right[j];
+            j += 1;
+        }
+        k += 1;
+    }
+
+    while i < n_left {
+        arr[k] = left[i];
+        i += 1;
+        k += 1;
+    }
+
+    while j < n_right {
+        arr[k] = right[j];
+        j += 1;
+        k += 1;
+    }
+}
+
+pub fn merge_sort(arr: &mut [i32], p: usize, r: usize) {
+    if p >= r {
+        return;
+    }
+    let q = (p + r) / 2;
+    merge_sort(arr, p, q);
+    merge_sort(arr, q + 1, r);
+    merge(arr, p, q, r);
+}
